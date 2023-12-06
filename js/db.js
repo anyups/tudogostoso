c9ec3f2ce89a433d7eeaf1d244914c67c69e4723
anyups <anyy.gabriele@gmail.com>
2023-12-06 14:10:07 -0400

    commit update 8

--- js/db.js
+++ js/db.js
@@ -1,5 +1,20 @@
 import { openDB } from "idb";
 
+
+//registrando a service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      try {
+        let reg;
+        reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+  
+        console.log('Service worker registrada! 😎', reg);
+      } catch (err) {
+        console.log('😥 Service worker registro falhou: ', err);
+      }
+    });
+  }
+  
 let db;
 async function bancoDB(){
     try {
@@ -24,17 +39,16 @@ async function bancoDB(){
 
 window.addEventListener('DOMContentLoaded', async event =>{
     bancoDB();
-    document.getElementById('cadastrar').addEventListener('click', novaReceita);
 });
 
-async function novaReceita() {
+async function novaReceita(foto) {
     let nome = document.getElementById("nome").value;
     let nomeDono = document.getElementById("nomeDono").value;
     let descricao = document.getElementById("descricao").value;
     const tx = await db.transaction('receita', 'readwrite')
     const store = tx.objectStore('receita');
     try {
-        await store.add({ nome: nome, descricao: descricao, nomeDono: nomeDono });
+        await store.add({ nome: nome, descricao: descricao, nomeDono: nomeDono, foto: foto });
         await tx.done;
         limparCampos();
         console.log('Receita adicionada com sucesso!');
@@ -91,5 +105,39 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
   });
+
+
+  // configurando as constraintes do video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger")
+
+//Estabelecendo o acesso a camera e inicializando a visualização
+function cameraStart() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      let track = stream.getTracks[0]
+      cameraView.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.error("Ocorreu um Erro.", error);
+    });
+}
+
+// Função para tirar foto
+cameraTrigger.onclick = function () {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+};
+
+// carrega imagem de camera quando a janela carregar
+window.addEventListener("load", cameraStart, false);
 
   
